80cfeec5ae9ed4eab65081d734420c9717bc95cc
feng <n/a>
2017-05-20 09:53:13 -0400

    refactoring

--- web/src/main/scala/gsl/web/GslApp.scala
+++ web/src/main/scala/gsl/web/GslApp.scala
@@ -19,20 +19,16 @@ object GslApp extends JSApp {
   var editing: Option[Item] = None
 
   def main: Unit = { $ { () =>
-    $("#item").hide()
 
     Ajax.get(s"http://$host/$list").
-      map(_.as[Shopping]).
-      foreach { s =>
+      map(_.as[Shopping]).foreach { s =>
         shopping = s
         bindList
       }
 
     $("#add").klick {
       editing = None
-      bindSingle
-      $("#remove").hide()
-      showSingle
+      bindShowOne
     }
 
     $("#cancel").klick {
@@ -44,31 +40,66 @@ object GslApp extends JSApp {
     }
 
     $("#save").klick {
-      val saved = editing match {
-        case Some(existing) => Ajax.put(s"http://$host/$list/$item/${existing.title}", write(toItem))
-        case None => Ajax.post(s"http://$host/$list/$item", write(toItem))
+      val it = Item($("#title").string, Option($("#notes").string).filterNot(_.isEmpty))
+      editing match {
+        case Some(_) => updateOne(it)
+        case None => createOne(it)
       }
-
-      saved.map(_.as[Shopping]).foreach { s =>
-        shopping = s
-        rebindList
-        showList
-    }}
+    }
 
     $("#remove").klick {
       if (window.confirm("Are you sure?")) {
-        editing match {
-          case Some(existing) =>
-            Ajax.delete(s"http://$host/$list/$item/${existing.title}").
-              map(_.as[Shopping]).foreach { s =>
-              shopping = s
-              rebindList
-              showList
-            }
-        }
+        removeOne
       }
     }
   }}
+
+  def bindShowOne: Unit = {
+    $("#title").value(editing.map(_.title).orNull)
+    $("#notes").value(editing.flatMap(_.notes).orNull)
+
+    $("#list").hide()
+    $("#item").show()
+
+    editing match {
+      case Some(_) => $("#remove").show()
+      case None => $("#remove").hide()
+    }
+    validate
+  }
+
+  def bindList: Unit = {
+    shopping.items.foreach { i =>
+      $("#items").append(
+        s"""
+           |<li>
+           |<span id="${i.title}" class="item">${i.title}</span>
+           |<span id="${i.title}" class="purchase">${if (i.purchased) "[X]" else "[ ]"}</span>
+           |</li>
+           |""".
+          stripMargin
+      )
+    }
+
+    $(".item").each { e: Element =>
+      $(e).klick {
+        editing = shopping.find(e.id)
+        bindShowOne
+      }
+    }
+
+    $(".purchase").each { e: Element =>
+      $(e).klick {
+        editing = shopping.find(e.id)
+        updateOne(editing.get.togglePurchased)
+      }
+    }
+  }
+
+  def showList: Unit = {
+    $("#item").hide()
+    $("#list").show()
+  }
 
   def validate: Unit = {
     val title = $("#title").string
@@ -80,61 +111,41 @@ object GslApp extends JSApp {
     $("#save").prop("disabled", missing || duplicated)
   }
 
-  def showSingle: Unit = {
-    $("#list").hide()
-    $("#item").show()
-  }
-
-  def showList: Unit = {
-    $("#item").hide()
-    $("#list").show()
-  }
-
-  def bindSingle: Unit = {
-    $("#title").value(editing.map(_.title).orNull)
-    $("#notes").value(editing.flatMap(_.notes).orNull)
-  }
-
-  def bindList: Unit = shopping.items.foreach { i =>
-    $("#items").append(
-      s"""
-         |<li>
-         |<span id="${i.title}" class="item">${i.title}</span>
-         |<span id="${i.title}" class="purchase">${if (i.purchased) "[X]" else "[ ]"}</span>
-         |</li>
-         |""".
-        stripMargin
-    )
-
-    $(".item").each { e: Element =>
-      $(e).klick {
-        editing = shopping.find(e.id)
-        bindSingle
-        validate
-        $("#remove").show()
-        showSingle
-      }
-    }
-
-    $(".purchase").each { e: Element =>
-      $(e).klick {
-        shopping.find(e.id).foreach { it =>
-          Ajax.put(s"http://$host/$list/$item/${it.title}", write(it.togglePurchased)).
-            map(_.as[Shopping]).foreach { s =>
-              shopping = s
-              rebindList
-              showList
-        }}
-      }
+  def createOne(toCreate: => Item): Unit = {
+    Ajax.post(s"http://$host/$list/$item", write(toCreate)).
+      map(_.as[Shopping]).foreach { s =>
+        shopping = s
+        rebindList
+        showList
     }
   }
 
-  def rebindList = {
+  def updateOne(toUpdate: => Item): Unit = {
+    editing.foreach { before =>
+      Ajax.put(s"http://$host/$list/$item/${before.title}", write(toUpdate)).
+        map(_.as[Shopping]).foreach { s =>
+          shopping = s
+          editing = None
+          rebindList
+          showList
+      }}
+  }
+
+  def removeOne: Unit = {
+    editing.foreach { existing =>
+      Ajax.delete(s"http://$host/$list/$item/${existing.title}").
+        map(_.as[Shopping]).foreach { s =>
+          shopping = s
+          editing = None
+          rebindList
+          showList
+      }}
+  }
+
+  def rebindList: Unit = {
     $("#items").empty()
     bindList
   }
-
-  def toItem = Item($("#title").string, Option($("#notes").string).filterNot(_.isEmpty))
 
   implicit class JQueryExt(jq: JQuery) {
     def string() = jq.value().toString.trim
